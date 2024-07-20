a1cf02df49fa0481fd7f1695987c5bbf5798cd07
sudipsaha06 <154732963+sudipsaha06@users.noreply.github.com>
2024-07-20 08:20:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vk:vkdatabase@cluster0.tnhlebr.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://vk:vkdatabase@cluster0.zk14c3i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "vk")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
